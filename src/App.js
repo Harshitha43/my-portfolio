70e11fbebbb35875e4eaf783f11d675502a13cbc
Reon Britto <reonbritto@gmail.com>
2024-10-26 02:16:27 +0530

    link name changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // src/App.js
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link,Navigate } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
 import Projects from './components/Projects';
